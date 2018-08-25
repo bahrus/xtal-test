3bd1d10e3d539bd53e5919918a591c5e09583d00
bahrus <anderson.bruce.b@gmail.com>
2018-08-25 18:44:45 -0400

    ...

--- index.js
+++ index.js
@@ -1,41 +1,47 @@
-// import { ConsoleMessage, Browser, LaunchOptions  } from "puppeteer"; //typescript
-// import {Test} from "tape";
-// const test = require('tape');
-// const puppeteer = require('puppeteer');
-// const path = require('path');
-// const TapeTestRunner = {
-//     test: test
-// } as Test;
-// (async () => {
-//     const launchOptions = {
-//         headless: true,
-//         args:['--allow-file-access-from-files']
-//     } as LaunchOptions;
-//     const browser = await puppeteer.launch(launchOptions) as Browser;
-//     const page = await browser.newPage();
-//     page.on('console', (msg: ConsoleMessage) => console.log('PAGE LOG:', msg.text()));
-//     const devFile = path.resolve(__dirname, '../demo/page-1c.html');
-//     await page.goto(devFile);
-//     //await delay(4000);
-//     await page.waitFor(4000);
-//     const textContent = await page.$eval('page-2c', (c: any) => c.shadowRoot.querySelector('page-3c'));
-//     await page.screenshot({path: 'example.png'});
-//     await browser.close();
-//     TapeTestRunner.test('testing dev.html', t => {
-//         t.ok(textContent);
-//         t.end();
-//     });
-//   })();
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const handler = require('serve-handler');
 const http = require('http');
-const server = http.createServer((request, response) => {
+let server = http.createServer((request, response) => {
     // You pass two more arguments for config and middleware
     // More details here: https://github.com/zeit/serve-handler#options
     return handler(request, response);
 });
+server = require('http-shutdown')(server);
 server.listen(3000, () => {
     console.log('Running at http://localhost:3000');
 });
-setTimeout(() => {
-    server.close();
-}, 10000);
+const test = require('tape');
+const puppeteer = require('puppeteer');
+const path = require('path');
+const TapeTestRunner = {
+    test: test
+};
+(async () => {
+    const launchOptions = {
+        headless: true,
+    };
+    const browser = await puppeteer.launch(launchOptions);
+    const page = await browser.newPage();
+    page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
+    //const devFile = path.resolve(__dirname, 'localhost:3000');
+    await page.goto('http://localhost:3000');
+    await page.screenshot({ path: 'example.png' });
+    server.shutdown(function () {
+        console.log('Everything is cleanly shutdown.');
+        process.exit();
+    });
+    //await delay(4000);
+    //await page.waitFor(4000);
+    // const textContent = await page.$eval('page-2c', (c: any) => c.shadowRoot.querySelector('page-3c'));
+    // await page.screenshot({path: 'example.png'});
+    // await browser.close();
+    // TapeTestRunner.test('testing dev.html', t => {
+    //     t.ok(textContent);
+    //     t.end();
+    // });
+})();
+//server.close();
+// setTimeout(() =>{
+//     server.close();
+// }, 10000);
